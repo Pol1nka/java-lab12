81e7fe5eb3bde202d9dd578b8459e87022de2e06
Polina Starodubtseva <akulam75@gmail.com>
2023-12-04 02:17:35 +0500

    test

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Integer int1 = new Integer(10);
+        String ss = "123";
+
+        System.out.println(int1.decode(ss));
+    }
+}
